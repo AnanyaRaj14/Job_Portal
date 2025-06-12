e2d8ad563ff0b5d9c1b8fc10dde370171fc9b555
AnanyaRaj14 <ananyam748@gmail.com>
2025-06-12 12:02:26 +0530

    first commit

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,75 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const register = async(req, res) => {
+    try{
+        const {fullname, email, phoneNumber, password, role} = req.body;
+        if(!fullname || !email || !phoneNumber || !password || !role){
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        };
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                message:'User already exist with this email.',
+                success:false,
+            })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            fullname,
+            email, 
+            phoneNumber,
+            password: hashedPassword, 
+            role,
+        }) 
+    } catch(error) {
+
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password, role} = req.body;
+        if(!email || !password || !role){
+            return res.status(400).json({
+                message: "Something is missing",
+                success: false
+            });
+        };
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                message:"Incorrect email or password.",
+                success: false,
+            })
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+         if(!isPasswordMatch){
+            return res.status(400).json({
+                message:"Incorrect email or password.",
+                success: false,
+            })
+        }
+        // check whether the role is provided correct or not
+        if(role != user.role){
+            return res.status(400).json({
+                message: "Account doesn't exists with the current role",
+                success: false
+            })
+        };
+        
+        const tokenData = {
+            userId:user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn:'Id'});
+
+        return res.status(200).cookie("token", token, {maxAge:1*24*60*60})
+
+    } catch(error){
+
+    }
+}
