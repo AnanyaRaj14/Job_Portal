4bc7e1457618e98e823f6721c9c84fb3ddd694d8
AnanyaRaj14 <ananyam748@gmail.com>
2025-07-28 17:44:42 +0530

    filterCard

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { RadioGroup } from '@radix-ui/react-radio-group';
+import { RadioGroupItem } from './ui/radio-group';
+
+
+const filterData = [
+    {
+        filterType: "Location",
+        array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Chennai", "Mumbai"]
+    },
+    {
+        filterType: "Industry",
+        array: ["Frontend Developer", "Backend Developer", "Data Science", "FullStack Developer", "Nextjs Developer"]
+    },
+    {
+        filterType: "Salary",
+        array: ["0 - 40k", "42k to 1lakh", "1lakh to 5lakh"]
+    },
+];
 
 const FilterCard = () => {
-  return (
-    <div>FilterCard</div>
-  )
+    return (
+        <div className='w-full bg-white p-3 rounded-md'>
+            <h1 className='font-bold text-lg'>Filter Jobs</h1>
+            <hr className='mt-3' />
+            <RadioGroup>
+                {
+                    filterData.map((data, index) => (
+                        <div>
+                            <h1 className='font-medium text-lg'>{data.filterType}</h1>
+                            {
+                                data.array.map((item, index) => {
+                                    return (
+                                        <div className="flex items-center space-x-2 my-2">
+                                            <RadioGroupItem value={item} />
+                                            <label>{item}</label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    ))
+                }
+            </RadioGroup>
+        </div>
+    )
 }
 
 export default FilterCard
--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Button } from './ui/button'
-import { Bookmark } from 'lucide-react'
+import { Badge, Bookmark } from 'lucide-react'
 import { Avatar, AvatarImage } from './ui/avatar'
 const Job = () => {
     return (
-        <div>
-            <div>
+        <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
+            <div className='flex items-center justify-between'>
                 <p className='text-sm text-gray-500'>5 days ago</p>
                 <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
             </div>
 
             <div className='flex items-center gap-2 my-2'>
-                
+
                 <Button className="p-6" variant="outline" size="icon">
                     <Avatar>
                         <AvatarImage src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/40-Brand-Logos-for-Creative-and-Cool-Companies/orange-electronics-company-by-ions-brandcrowd.png" />
@@ -19,18 +19,19 @@ const Job = () => {
                 </Button>
 
                 <div>
-                    <h1>Company Name</h1>
-                    <p>India</p>
-                </div>
-
-            </div>
-
-
-
-            {/* <div>
-                    <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
+                    <h1 className='font-medium text-lg'>Company Name</h1>
                     <p className='text-sm text-gray-500'>India</p>
-                </div> */}
+                </div>
+            </div>
+            <div>
+                <h1 className='font-bold text-lg my-2'>Title</h1>
+                <p className='text-sm text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nesciunt autem nulla reiciendis sapiente porro nam laboriosam perferendis neque debitis.</p>
+            </div>
+           
+            <div className='mt-2'>
+                <Button  variant="outline">Details</Button>
+                <Button className="bg-[#7209b7] mx-3">Save For Later</Button>
+            </div>
         </div>
 
     )
