04c066253f87c9d9600c1199748ef56d3a5222c9
AnanyaRaj14 <ananyam748@gmail.com>
2025-07-31 11:04:46 +0530

    update profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './shared/Navbar'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
@@ -6,11 +6,14 @@ import { Pen, Mail, Contact } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
 import AppliedJobTable from './AppliedJobTable'
+import UpdateProfileDialog from './UpdateProfileDialog'
 
 const skills = ["html", "css", "Js", "reactJs", "MongoDB", "ExpressJs", "NodeJs"];
+const isResume = true;
 
 const Profile = () => {
-    const isResume = true;
+    const [open, setOpen] = useState(false);
+
     return (
         <div>
             <Navbar />
@@ -27,7 +30,10 @@ const Profile = () => {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, nesciunt.</p>
                         </div>
                     </div>
-                    <Button className='text-right' variant='outline'><Pen /></Button>
+                    <Button
+                        onClick={() => setOpen(true)}
+                        className='text-right'
+                        variant='outline'><Pen /></Button>
                 </div>
                 <div className='my-5'>
                     <div className='flex items-center gap-3 my-2'>
@@ -55,10 +61,11 @@ const Profile = () => {
                 </div>
             </div>
             <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
-                    <h1  className='text-xl font-bold p-5'>Applied Jobs</h1>
-                    {/*  Total No. of Application Table */}
-                    <AppliedJobTable />
-                </div>
+                <h1 className='text-xl font-bold p-5'>Applied Jobs</h1>
+                {/*  Total No. of Application Table */}
+                <AppliedJobTable />
+            </div>
+            <UpdateProfileDialog open={open} setOpen={setOpen}/>
         </div>
     )
 }
