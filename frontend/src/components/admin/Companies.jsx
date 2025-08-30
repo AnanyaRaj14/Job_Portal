72e4f7cc4f2a49f7ab137f37ad5db81f409874e0
AnanyaRaj14 <ananyam748@gmail.com>
2025-08-30 13:10:54 +0530

    companies auth

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -3,8 +3,10 @@ import Navbar from '../shared/Navbar'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import CompaniesTable from './CompaniesTable'
+import { useNavigate } from 'react-router-dom'
 
 const Companies = () => {
+    const navigate = useNavigate();
     return (
         <div>
             <Navbar />
@@ -15,7 +17,7 @@ const Companies = () => {
                         placeholder="Filter by name"
                         onChange={(e) => setInput(e.target.value)}
                     />
-                    <Button>New Company</Button>
+                    <Button onClick={() => navigate("/admin/companies/create")}>New Company</Button>
                 </div>
                 <CompaniesTable />
             </div>
--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Edit2, MoreHorizontal } from 'lucide-react'
 
 const CompaniesTable = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Table>
+                <TableCaption>List of your registered companies</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Logo</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead className="text-right">Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableCell>
+                        <Avatar>
+                            <AvatarImage src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/40-Brand-Logos-for-Creative-and-Cool-Companies/orange-electronics-company-by-ions-brandcrowd.png" />
+                        </Avatar>
+                    </TableCell>
+                    <TableCell>Company Name</TableCell>
+                    <TableCell>30-08-2025</TableCell>
+                    <TableCell className="text-right cursor-pointer">
+                        <Popover>
+                            <PopoverTrigger><MoreHorizontal/></PopoverTrigger>
+                            <PopoverContent className="w-32">
+                                <div className='flex items-center gap-2 w-fit cursor-pointer'>
+                                    <Edit2 className='w-4'/>
+                                    <span>Edit</span>
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+                    </TableCell>
+                </TableBody>
+            </Table>
+        </div>
+    )
 }
 
 export default CompaniesTable
