7737ced971a14b621be085406afafeddb3edd292
AnanyaRaj14 <ananyam748@gmail.com>
2025-08-31 17:08:27 +0530

    company Setup ui

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Button } from '../ui/button'
+import { ArrowLeft } from 'lucide-react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
 
 const CompanySetup = () => {
+  const [input, setInput] = useState({
+    name: "",
+    description: "",
+    website: "",
+    location: "",
+    file: null
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  }
+
+  const changeFileHandler = (e) => {
+    const file = e.target.files?.[0];
+    setInput({ ...input, file });
+  }
+
   return (
     <div>
-      <h1>CompanySetup</h1>
+      <Navbar />
+      <div className='max-w-xl mx-auto my-10'>
+        <form action="">
+          <div className='flex items-center gap-5 p-8'>
+            <Button varient="outlin e" className="flex items-center gap-2 text-gray-500 font-semibold">
+              <ArrowLeft />
+              <span>Back</span>
+            </Button>
+            <h1 className='font-bold text-xl'>Company Setup</h1>
+          </div>
+          <div className='grid grid-cols-2 gap-4'>
+            <div>
+              <Label>Company Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={input.name}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={input.description}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Website</Label>
+              <Input
+                type="text"
+                name="website"
+                value={input.website}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Location</Label>
+              <Input
+                type="text"
+                name="location"
+                value={input.location}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Logo</Label>
+              <Input
+                type="file"
+                accept="image/*"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+
+        </form>
+      </div>
+
     </div>
   )
 }
