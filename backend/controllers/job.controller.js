a9fdf554a9502109be34028ccb220b6b3567659b
AnanyaRaj14 <ananyam748@gmail.com>
2025-08-31 16:18:12 +0530

    job controller logic

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -69,13 +69,9 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(req.params.id).populate({
-            path:"applications"
-        })
-            // .populate("company", "name location")   // only fetch needed fields
-            // .populate("applications");  // optional
-
-            console.log('jobs in backend : ', job);
+        const job = await Job.findById(jobId)
+            .populate("applications")
+            .populate("company", "companyName location website");
 
         if (!job) {
             return res.status(404).json({
