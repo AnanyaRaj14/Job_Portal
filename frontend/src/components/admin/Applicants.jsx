6746eb7b3bdc3e4d4e5eca8d21201a19ba4f12b1
AnanyaRaj14 <ananyam748@gmail.com>
2025-10-02 00:32:48 +0530

    application table and it's slice

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import Navbar from '../shared/Navbar'
 import ApplicantsTable from './ApplicantsTable'
+import axios from 'axios'
+import { APPLICATION_API_END_POINT } from '@/utils/constant'
+import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setAllApplicants } from '@/redux/applicationSlice'
+
 
 const Applicants = () => {
+    const params = useParams();
+    const dispatch  = useDispatch();
+    useEffect(() => {
+        const fetchAllApplicants = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
+                if(res.data.success){
+                    dispatch(setAllApplicants(res.data.job));
+                }     
+            }catch (error){
+                 console.log(error);
+            }
+        }
+        fetchAllApplicants();
+    }, []);
+
   return (
     <div>
       <Navbar />
--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -1,24 +1,56 @@
 import React from 'react'
-import { Table, TableCaption, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { MoreHorizontal } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+
+const shortlistingStatus = ["Accepted", "Rejected"];
 
 const ApplicantsTable = () => {
     return (
         <div>
             <Table>
                 <TableCaption>A list of your recent applied user</TableCaption>
+
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>FullName</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Contact</TableHead>
+                        <TableHead>Resume</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead className="text-right">Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                    <TableRow>
+                        <TableCell>Ananya Mishra</TableCell>
+                        <TableCell>ananya@test.com</TableCell>
+                        <TableCell>9735264795006847</TableCell>
+                        <TableCell>Resume</TableCell>
+                        <TableCell>01/10/2025</TableCell>
+                        <TableCell className="float-right cursor-pointer">
+                            <Popover>
+                                <PopoverTrigger>
+                                    <MoreHorizontal />
+                                </PopoverTrigger>
+                                <PopoverContent className="w-32">
+                                    {
+                                        shortlistingStatus.map((status, index) => {
+                                            return (
+                                                <div key={index} className='flex w-fit items-center my-2 cursor-pointer'>
+                                                    <span>{status}</span>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </PopoverContent>
+                            </Popover>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
             </Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>FullName</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead>Contact</TableHead>
-                    <TableHead>Resume</TableHead>
-                    <TableHead>Date</TableHead>
-                    <TableHead className="text-right">Action</TableHead>
-                </TableRow>
-            </TableHeader>
-            
-        </div>
+        </div >
     )
 }
 
